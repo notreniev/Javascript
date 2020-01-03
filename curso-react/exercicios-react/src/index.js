43f6b9d718141c085d4ca5efe05c02b341fba7df
notreniev <everton.canez@gmail.com>
2020-01-03 13:43:49 -0300

    Some Refactories

---
+++ curso-react/exercicios-react/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const elemento = document.getElementById('root')
+const hello = <h1> Olá, React III</h1>
+ReactDOM.render(hello, elemento)
